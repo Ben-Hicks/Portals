3dd404e2c92efadf7528b53f122e32d65389c6dd
Ben-Hicks <31557370+Ben-Hicks@users.noreply.github.com>
2019-09-25 13:21:52 -0400

    Init

---
+++ Assets/Scripts/Library/Library.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Library{
+    
+
+
+    public static float LerpSmoothIn(float a, float b, float fProgress) {
+        return Mathf.Lerp(a, b, Mathf.Sin(fProgress * Mathf.PI * 0.5f));
+    }
+
+    public static float LerpSmoothOut(float a, float b, float fProgress) {
+        return Mathf.Lerp(a, b, 1f - Mathf.Cos(fProgress * Mathf.PI * 0.5f));
+    }
+
+    public static float LerpSmoothStep(float a, float b, float fProgress) {
+        return Mathf.Lerp(a, b, fProgress * fProgress * (3f - 2f * fProgress));
+    }
+
+    public static Vector2 LerpSmoothIn(Vector2 a, Vector2 b, float fProgress) {
+        return Vector2.Lerp(a, b, Mathf.Sin(fProgress * Mathf.PI * 0.5f));
+    }
+
+    public static Vector2 LerpSmoothOut(Vector2 a, Vector2 b, float fProgress) {
+        return Vector2.Lerp(a, b, 1f - Mathf.Cos(fProgress * Mathf.PI * 0.5f));
+    }
+
+    public static Vector2 LerpSmoothStep(Vector2 a, Vector2 b, float fProgress) {
+        return Vector2.Lerp(a, b, fProgress * fProgress * (3f - 2f * fProgress));
+    }
+}
---
+++ Assets/Scripts/Library/Singleton.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Need to maintain a list of all currently active Singleton classes
+//Singleton classes are registered when they are added as a component of an object in the current scene
+//switching out of a scene will delete all objects within that scene
+//(this might nullify some of the singleton static instances)
+
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+
+    private static T inst;
+    private bool bStarted = false;
+
+    public static T Get() {
+
+        if (inst == null) {
+            //Debug.LogError("Error! Static instance not set!");
+        }
+
+        return inst;
+    }
+
+    protected virtual void Awake() {
+
+        if (inst != null) {
+            //If an static instance exists,
+            // then panic!  Destroy ourselves
+            Debug.LogError("Warning!  This singleton already exists (" + inst.name + "), so we shouldn't instantiate a new one");
+            Destroy(gameObject);
+
+        } else {
+            inst = gameObject.GetComponent<T>();
+            //Debug.Log("init inst for type " + GetType().ToString());
+        }
+
+
+    }
+
+    //Singletons will Initialize themselves once when they're created (but will only get to exist if another instance doesn't already exist
+    // - switching scenes will delete the previous instance and let a new one Init itself and become the new singleton instance
+    public abstract void Init();
+
+    public virtual void Start() {
+
+        if (bStarted == true) return;
+        bStarted = true;
+
+        Init();
+
+    }
+
+
+}
