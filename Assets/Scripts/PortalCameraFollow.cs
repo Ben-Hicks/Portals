ad8296038b7165e1b5158ebbb9d89b182cb5936f
Ben-Hicks <31557370+Ben-Hicks@users.noreply.github.com>
2019-09-26 16:28:44 -0400

    Basic 2D portal placement working

--- Assets/Scripts/PortalCameraFollow.cs
+++ Assets/Scripts/PortalCameraFollow.cs
@@ -15,13 +15,21 @@ public class PortalCameraFollow : MonoBehaviour {
     void Update() {
         Vector3 v3PlayerOffsetFromPortal = portOwner.player.transform.position - portOwner.portalLinked.transform.position;
 
-        this.transform.position = portOwner.transform.position + v3PlayerOffsetFromPortal;
+        //Think about making the third parameter a cross product
+        float fAngularDifferenceBetweenPortals = Vector3.SignedAngle(portOwner.transform.forward, portOwner.portalLinked.transform.forward, Vector3.up);
+        float fRecip = Vector3.SignedAngle(portOwner.portalLinked.transform.forward, portOwner.transform.forward, Vector3.up);
 
-        float fAngularDifferenceBetweenPortals = Quaternion.Angle(portOwner.transform.rotation, portOwner.portalLinked.transform.rotation);
+       
+        Debug.Log(fAngularDifferenceBetweenPortals + " " + fRecip);
 
         Quaternion qPortalRotationDifference = Quaternion.AngleAxis(fAngularDifferenceBetweenPortals, Vector3.up);
+        Quaternion qPortalRotationRecip = Quaternion.AngleAxis(fRecip, Vector3.up);
 
-        Vector3 v3NewCameraDirection = qPortalRotationDifference * portOwner.player.transform.forward;
-        this.transform.rotation = Quaternion.LookRotation(v3NewCameraDirection, Vector3.up);
+        Vector3 v3NewCameraDirection = qPortalRotationRecip * portOwner.player.transform.forward;
+
+        v3PlayerOffsetFromPortal = qPortalRotationRecip * -v3PlayerOffsetFromPortal;
+        this.transform.position = portOwner.transform.position + v3PlayerOffsetFromPortal;
+
+        this.transform.rotation = Quaternion.LookRotation(-v3NewCameraDirection, Vector3.up);
     }
 }
